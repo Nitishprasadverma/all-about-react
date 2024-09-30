410048801d206d247b10dd726c0a434f18957fc7
Nitishprasadverma <nitishverma773964@gmail.com>
2024-09-30 22:45:20 +0530

    editable list item

--- react/odin-react/odin-projects/src/component/select.jsx
+++ react/odin-react/odin-projects/src/component/select.jsx
@@ -1,38 +1,88 @@
 import { useState } from "react";
 
 const intitalItems = [
-    {title: 'peretzels', id:0},
-    {title: 'cripsy seaweed', id:1},
-    {title:'granola bar', id:2},
+    { title: 'peretzels', id: 0 },
+    { title: 'cripsy seaweed', id: 1 },
+    { title: 'granola bar', id: 2 },
 ]
 
+// function Menu() {
+//     const [items, setItems] = useState(intitalItems);
+//     const [selectedItem, setSelectedItem] = useState(items[0]);
+
+//     return (
+// <>
+
+// <h2>What's your travel snack</h2>
+
+//         <ul>
+
+//             {items.map(item =>(               <li key={item.id}
+//                 >
+//                     {item.title}
+//                     {' '}
+//                     <button onClick={() =>{
+//                         setSelectedItem(item)
+//                     }}>Choose</button>
+
+//                 </li>
+//         ))}
+//         </ul>
+
+//         <p>You picked {selectedItem.title}.</p>
+
+// </>
+
+//     )
+// }
+
+
+// editable menu function
+
 function Menu() {
-    const [items, setItems] = useState(intitalItems);
+    const [items, setItmes] = useState(intitalItems);
     const [selectedItem, setSelectedItem] = useState(items[0]);
 
+    function handleItemChange(id, e) {
+        setItmes(items.map(item => {
+            if (item.id === id) {
+                return {
+                    ...item,
+                    title: e.target.value
+                }
+            }
+            else {
+                return item;
+            }
+        }))
+    }
+
     return (
-<>
+        <>
+            <h2>What's your travel snack?</h2>
 
-<h2>What's your travel snack</h2>
+            <ul>
 
-        <ul>
+                {items.map((item, index) => (
+                    <li key={item.id}>
+                        <input
+                            value={item.title}
+                            onChange={e => {
+                                handleItemChange(item.id, e)
+                            }}
+                        />
+                        {' '}
+                        <button onClick={() => {
+                            setSelectedItem(item);
+                        }} >Choose</button>
 
-            {items.map(item =>(               <li key={item.id}
-                >
-                    {item.title}
-                    {' '}
-                    <button onClick={() =>{
-                        setSelectedItem(item)
-                    }}>Choose</button>
+                    </li>
+                ))}
+            </ul>
 
-                </li>
-        ))}
-        </ul>
+            <p>You Picked {selectedItem.title}</p>
 
-        <p>You picked {selectedItem.title}.</p>
-
-</>
-        
+        </>
     )
 }
 
