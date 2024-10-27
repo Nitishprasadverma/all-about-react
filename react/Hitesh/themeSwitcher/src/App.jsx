d110654175055a99f373f727c629fe8f43b98043
Nitishprasadverma <nitishverma773964@gmail.com>
2024-10-27 23:59:05 +0530

    themechanger

---
+++ react/Hitesh/themeSwitcher/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='p-4, bg-orange-800 text-lime-400'>hello</h1>
+    </>
+  )
+}
+
+export default App
