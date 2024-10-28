a6039a06fc06e28cebac82f26d5cecdb966088da
Nitishprasadverma <nitishverma773964@gmail.com>
2024-10-28 13:54:34 +0530

    card-completed

--- react/Hitesh/themeSwitcher/src/App.jsx
+++ react/Hitesh/themeSwitcher/src/App.jsx
@@ -1,14 +1,45 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import './App.css'
+import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/ThemeBtn'
+import Card from './components/card'
 
 function App() {
-  const [count, setCount] = useState(0)
+const [themeMode, setThemeMode] = useState("light")
 
+const lightMode = () =>{
+setThemeMode("light")
+}
+const darkMode = () =>{
+  setThemeMode("dark")
+}
+
+// actual change in theme
+
+const background = document.querySelector('html');
+useEffect(() =>{
+
+background.classList.remove("light", "dark");
+background.classList.add(themeMode);
+},[themeMode])
   return (
-    <>
-      <h1 className='p-4, bg-orange-800 text-lime-400'>hello</h1>
-    </>
+    <ThemeProvider value={{themeMode,lightMode,darkMode}}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            {/* theme button */}
+
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            {/* card */}
+            <Card />
+          </div>
+        </div>
+      </div>
+    </ThemeProvider>
   )
 }
 
